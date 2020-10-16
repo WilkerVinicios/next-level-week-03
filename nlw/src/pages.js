f1c401ce5b96342a279fcabf5bde21a5b7f9d3d8
Wilker Vinicios <willkervinicios@gmail.com>
2020-10-16 20:44:51 -0300

    Quinto dia;
    Finalizando Projeto;
    Criando banco de dados com sqlite;
    Criar metodos get e post;
    Alterar paginas para page builder;
    Adicionado function saveOrphanage;
    Alterado server.js;
    Alterado pages.js;

--- nlw/src/pages.js
+++ nlw/src/pages.js
@@ -1,4 +1,5 @@
-const orphanages = require('./database/fakedata.js');
+const Database = require('./database/db');
+const saveOrphanage = require('./database/saveOrphanage')
 
 module.exports = {
     
@@ -6,15 +7,64 @@ module.exports = {
         return response.render('index')
     },
     
-    orphanage(request, response){
-        return response.render('orphanage')
+    async orphanage(request, response){
+        const id = request.query.id
+        try {
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id ="${id}"`)
+            const orphanage = results[0]
+
+            orphanage.images = orphanage.images.split(',')
+            orphanage.firstImage = orphanage.images[0]
+            
+            orphanage.open_on_weekends = (orphanage.open_on_weekends == "1")? true : false;
+            return response.render('orphanage', {orphanage})
+        } catch (error) {
+            console.log(error)
+            return response.send('Erro no banco de dados!')
+        }
     },
 
-    orphanages(request, response){
-        return response.render('orphanages', {orphanages})
+    async orphanages(request, response){
+        try {
+            const db = await Database;
+            const orphanages = await db.all('SELECT * FROM orphanages')
+            return response.render('orphanages', {orphanages})
+        } catch (error) {
+            console.log(error)
+            return response.send('Erro no banco de dados!')
+        } 
     },
 
     createOrphanage(request, response){
         return response.render('create-orphanage')
+    },
+
+    async saveOrphanage(request, response){
+        const field = request.body
+
+        if(Object.values(field).includes('')){
+            return response.send('Todos os campos devem ser Preenchidos')
+        }
+        //salvar um orfanato
+        try {
+            const db = await Database
+            await saveOrphanage(db, {
+            lat: field.lat,
+            lng: field.lng,
+            name: field.name,
+            about: field.about,
+            whatsapp: field.whatsapp,
+            images: field.images.toString(),
+            instructions: field.instructions,
+            opening_hours: field.opening_hours,
+            open_on_weekends: field.open_on_weekends,
+        })
+            //redirecionar 
+            return response.redirect('/orphanages')
+        } catch (error) {
+            console.log(error)
+            return response.send('Erro no banco de dados!')
+        }
     }
 }
